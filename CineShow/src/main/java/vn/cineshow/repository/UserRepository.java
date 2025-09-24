a0d27c03fa3f214f7ed5af57b44fb9b679347208
Win 11 <vuphuc2057@gmail.com>
2025-09-24 22:23:36 +0700

    create API: change- password and forgot- password( not OTP)

--- CineShow/src/main/java/vn/cineshow/repository/UserRepository.java
+++ CineShow/src/main/java/vn/cineshow/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package vn.cineshow.repository;
 
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import vn.cineshow.model.User;
 
-public interface UserRepository extends Repository<User, Long> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByAccount_Email(String email);
 }
+
