d7c62be1ea4071501b52aba7482b33a7d0b53c91
gekok <172472094+gekok@users.noreply.github.com>
2025-09-25 08:00:12 +0700

    create API: change- password and forgot- password( not OTP) (#10)

---
+++ CineShow/src/main/java/vn/cineshow/dto/request/ForgotPasswordRequest.java
@@ -0,0 +1,23 @@
+package vn.cineshow.dto.request;
+
+public class ForgotPasswordRequest {
+    private String email;
+    private String newPassword;
+
+    // Getters & Setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
