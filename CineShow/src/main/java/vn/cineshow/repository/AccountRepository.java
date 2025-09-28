1ed87e7f0798cf3580474c5eab10cc4c5fae1f17
Your Name <huyenfilw642@gmail.com>
2025-09-28 15:07:28 +0700

    save before branch change

--- CineShow/src/main/java/vn/cineshow/repository/AccountRepository.java
+++ CineShow/src/main/java/vn/cineshow/repository/AccountRepository.java
@@ -11,7 +11,6 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<UserDetails> findByEmail(String email);
 
     Account getAccountByEmail(String email);
-    Optional<Account> findByEmailAndStatus(String email, AccountStatus status);
 
     Optional<Account> findAccountByEmail(String email);
 
