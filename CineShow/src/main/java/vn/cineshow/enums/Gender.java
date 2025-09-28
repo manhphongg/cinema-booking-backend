5df0166d36267991357d04bb56600007265309d8
Your Name <huyenfilw642@gmail.com>
2025-09-29 00:21:55 +0700

    add annotation Json.. in Gender

--- CineShow/src/main/java/vn/cineshow/enums/Gender.java
+++ CineShow/src/main/java/vn/cineshow/enums/Gender.java
@@ -1,7 +1,14 @@
 package vn.cineshow.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Gender {
-MALE,
-FEMALE,
-OTHER
+    MALE,
+    FEMALE,
+    OTHER;
+
+    @JsonCreator
+    public static Gender fromString(String value) {
+        return value == null ? null : Gender.valueOf(value.toUpperCase());
+    }
 }
