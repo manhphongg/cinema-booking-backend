f3e0c1a5f6def817a17bf26d9d24d61a9292b103
ohho <hungbx4869@gmail.com>
2025-09-29 22:27:32 +0700

    hungnthe (#28)
    * Hungnthe: get User Profile
    edit User Profile
    
    * Hungnthe: get User Profile
    edit User Profile
    
    * Hungnthe: get User Profile
    edit User Profile

--- CineShow/src/main/java/vn/cineshow/dto/response/UserResponse.java
+++ CineShow/src/main/java/vn/cineshow/dto/response/UserResponse.java
@@ -1,4 +1,18 @@
 package vn.cineshow.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private String address;
+    private int loyalPoint;
 }
